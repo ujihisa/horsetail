5d0c015b9d018008c31e3ec1bd30958def7a8246
ujihisa <ujihisa@gmail.com>
2012-04-20 18:24:12 -0700

    mostly from mario-bukkit

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject horsetail "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.3.1"]
+                 [swank-clojure/swank-clojure "1.3.3"]
+                 [cloft "1.0.0-SNAPSHOT"]])
--- src/horsetail/core.clj
+++ src/horsetail/core.clj
@@ -1,1 +1,23 @@
-(ns horsetail.core)
+(ns horsetail.core
+  (:require [cloft.cloft :as c])
+  (:require [swank.swank])
+  (:require [clojure.set :as s])
+  (:import [org.bukkit Bukkit Material])
+  (:import [org.bukkit.entity Animals Arrow Blaze Boat CaveSpider Chicken
+            ComplexEntityPart ComplexLivingEntity Cow Creature Creeper Egg
+            EnderCrystal EnderDragon EnderDragonPart Enderman EnderPearl
+            EnderSignal ExperienceOrb Explosive FallingSand Fireball Fish
+            Flying Ghast Giant HumanEntity Item LightningStrike LivingEntity
+            MagmaCube Minecart Monster MushroomCow NPC Painting Pig PigZombie
+            Player PoweredMinecart Projectile Sheep Silverfish Skeleton Slime
+            SmallFireball Snowball Snowman Spider Squid StorageMinecart
+            ThrownPotion TNTPrimed Vehicle Villager WaterMob Weather Wolf
+            Zombie])
+  (:import [org.bukkit.inventory ItemStack])
+  (:import [org.bukkit.event.entity EntityDamageByEntityEvent
+            EntityDamageEvent$DamageCause]))
+
+(defonce swank* nil)
+(defn on-enable [plugin]
+  (when (nil? swank*)
+    (def swank* (swank.swank/start-repl 4009))))
