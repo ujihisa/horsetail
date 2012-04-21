703c64509884a2c9c48343feadc434ed428e6e89
ujihisa <ujihisa@gmail.com>
2012-04-20 18:22:16 -0700

    first commit

---
+++ src/horsetail/core.clj
@@ -0,0 +1,1 @@
+(ns horsetail.core)
---
+++ test/horsetail/test/core.clj
@@ -0,0 +1,6 @@
+(ns horsetail.test.core
+  (:use [horsetail.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
